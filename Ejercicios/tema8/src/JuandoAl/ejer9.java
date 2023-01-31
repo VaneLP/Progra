28881ad7c68118884e1d050ac9f4dcd509615469
Vane <vanessa-lopez9@hotmail.com>
2023-01-31 14:19:56 +0100

    avances

--- Ejercicios/tema8/src/JuandoAl/ejer9.java
+++ Ejercicios/tema8/src/JuandoAl/ejer9.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ejer9 {
-    public int[][] lineas (int[][] matriz){
-        ArrayList<Integer> lista= new ArrayList<>();
-        for (int i=0;i< matriz.length;i++){
-
-        }
-        return ArrayList;
-    }
+//    public int[][] lineas (int[][] matriz){
+//        ArrayList<Integer> lista= new ArrayList<>();
+//        for (int i=0;i< matriz.length;i++){
+//
+//        }
+//        return ArrayList;
+//    }
 
 
 }
--- Ejercicios/tema8/src/Once/ejer11.java
+++ Ejercicios/tema8/src/Once/ejer11.java
@@ -1,4 +1,7 @@
 package Once;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 /*Realiza un programa que construya una matriz de 3x3 y la rellene aleatoriamente de
 números 0 o 1. El programa mostrará el tablero que se ha generado y cuantas ‘3 en raya’
 tienen los 0’s y cuantas tienen los 1’s, indicando quien en el ganador y si han empatado.
@@ -9,4 +12,23 @@ Por ejemplo, dada esta matriz, mostrará Lineas 0: 2 Lineas 1: 0 Gana 0
 0 1 0
 0 0 0*/
 public class ejer11 {
+    public static void main(String[] args) {
+        int[][] matriz = new int[3][3];
+        int nAle;
+
+        for (int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                nAle= ThreadLocalRandom.current().nextInt(0,1+1);
+
+                matriz[i][j]=nAle;
+            }
+        }
+
+        for (int[] mostrar1: matriz) {
+            System.out.println();
+            for (int mostrar2 : mostrar1) {
+                System.out.print(mostrar2 + " ");
+            }
+        }
+    }
 }

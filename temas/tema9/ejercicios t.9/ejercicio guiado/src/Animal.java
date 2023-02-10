0bf3ed76a326097b49df6173f5c85606fb40a002
VaneLP <vanessa-lopez9@hotmail.com>
2023-02-10 12:45:18 +0100

    ejer guiado

---
+++ temas/tema9/ejercicios t.9/ejercicio guiado/src/Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+    //atributos
+    String nombre;
+    int numEstancia;
+
+    //constructor
+
+    public Animal(String nombre, int numEstancia) {
+        this.nombre = nombre;
+        this.numEstancia = numEstancia;
+    }
+
+    //metodos
+
+
+}

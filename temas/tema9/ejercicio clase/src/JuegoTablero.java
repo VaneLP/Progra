9f12b85759cb57060cca34e2b44e20aaa5a78dbc
VaneLP <vanessa-lopez9@hotmail.com>
2023-02-21 09:07:56 +0100

    coments

--- temas/tema9/ejercicio clase/src/JuegoTablero.java
+++ temas/tema9/ejercicio clase/src/JuegoTablero.java
@@ -1,37 +1,37 @@
 public interface JuegoTablero {
     //metodos
-    int filas();
-
-    char columnas();
-
-    /**
-     * @param indice Pasara un indice numerico a su letra correspondiente del tablero
-     * @return una letra
-     */
-    char indiceAPosicion(int indice);
-
-    /**
-     * @param indice pasará un índice numérico a su posición numérica del tablero
-     * @return un numero
-     */
-    int indiceAPosicionN(int indice){
-
-    }
-
-    /**
-     * @param posicion pasará una letra que indica una posición en el tablero a su correspondiente índice
-     * @return un numero
-     */
-    int posicionAIndice(char posicion){
-
-    }
-
-    /**
-     * @param posicion pasará una posición númerica en el tablero a su correspondiente índice
-     * @return un numero
-     */
-    int posicionAIndice(int posicion){
-
-    }
+//    int filas();
+//
+//    char columnas();
+//
+//    /**
+//     * @param indice Pasara un indice numerico a su letra correspondiente del tablero
+//     * @return una letra
+//     */
+//    char indiceAPosicion(int indice);
+//
+//    /**
+//     * @param indice pasará un índice numérico a su posición numérica del tablero
+//     * @return un numero
+//     */
+//    int indiceAPosicionN(int indice){
+//
+//    }
+//
+//    /**
+//     * @param posicion pasará una letra que indica una posición en el tablero a su correspondiente índice
+//     * @return un numero
+//     */
+//    int posicionAIndice(char posicion){
+//
+//    }
+//
+//    /**
+//     * @param posicion pasará una posición númerica en el tablero a su correspondiente índice
+//     * @return un numero
+//     */
+//    int posicionAIndice(int posicion){
+//
+//    }
 
 }

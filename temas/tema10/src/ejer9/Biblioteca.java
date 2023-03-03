e1d3a73888efd8fee72714c4cfd2b36fda113545
VaneLP <vanessa-lopez9@hotmail.com>
2023-03-03 12:01:17 +0100

    continuacion ejercicio 9

---
+++ temas/tema10/src/ejer9/Biblioteca.java
@@ -0,0 +1,63 @@
+package ejer9;
+
+import ejer9.Excepciones.PublicationException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Biblioteca {
+    //Atributo
+    private ArrayList<Publicacion> listaPublicaciones =  new ArrayList<Publicacion>();
+
+    //metodos
+    public void anyadir(Publicacion publi){
+        listaPublicaciones.add(publi);
+    }
+
+    public void anyadirLibro(String isbn, String titulo, String autor) {
+        try{
+            Libro libri= new Libro(isbn, titulo,autor);
+        }
+        catch (PublicationException pE){
+            pE.getMessage();
+        }
+    }
+
+    public void buscar(String tituloBuscar) throws Exception{
+
+            throw new Exception("El titulo "+tituloBuscar+" no a sido encontrado");
+    }
+
+    public boolean buscarLibro(Libro l){
+        for (Publicacion listaPublicacion : listaPublicaciones) {
+            if(l.equals(listaPublicacion))
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+    public ArrayList<Revista> revistasOrdenadas(){
+        Collections.sort(revistasOrdenadas());
+
+        return revistasOrdenadas();
+    }
+
+    public void ordenar(){
+
+    }
+    /*
+    para ordenar por orden alfabetico tenemos que usar collections.sort(nombreArrayList);
+    Y para que funcione a la clase hay que implements comparable
+    */
+
+    public void mostrarLineas(){
+        for (Publicacion listaPublicacion : listaPublicaciones) {
+            System.out.println(listaPublicacion);
+        }
+
+    }
+
+}
--- temas/tema10/src/ejer9/Revista.java
+++ temas/tema10/src/ejer9/Revista.java
@@ -2,10 +2,11 @@ package ejer9;
 
 import ejer9.Excepciones.PublicationException;
 
-public class Revista{
+import java.util.Objects;
+
+public class Revista extends Publicacion implements Comparable<Revista>{
     //atributos
-    private final String issn;
-    private final String nombre;
+    private final String issn, nombre;
     private final int numero;
 
     //constructor
@@ -18,6 +19,47 @@ public class Revista{
         else{
             throw new PublicationException();
         }
+    }
+
+    //getters
+    //issn
+    public String getIssn() {
+        return issn;
+    }
+
+    //nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    //numero
+    public int getNumero() {
+        return numero;
+    }
+
+    //metodos
+    @Override
+    public String mostrarEnLinea() {
+        return String.format("%15s, %15s, %15s, %15s","ID|", "issn|","nombre|","numero|"+
+                "%15s, %15s, %15s, %15s",this.idUnico,this.issn,this.nombre,this.numero);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Revista revista = (Revista) o;
+        return Objects.equals(issn, revista.issn) && Objects.equals(nombre, revista.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(issn, nombre);
+    }
+
+    @Override
+    public int compareTo(Revista r) {
+        return nombre.compareTo(r.getNombre());
     }
 
 }

17d36d39e4186f3534c355a22fa2a90005648c8c
Vanessa Lopez <vanessa-lopez9@hotmail.com>
2023-04-24 12:19:30 +0200

    Progra ejer

--- temas/tema12/src/ejr4/EjerA4.java
+++ temas/tema12/src/ejr4/EjerA4.java
@@ -5,24 +5,44 @@ import java.io.File;
 public class EjerA4 {
     public static void main(String[] args) {
         //crear carpeta mis cosas
-        File misCosas = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\DOCS\\Mis Cosas");
-        System.out.println(misCosas.mkdir());
+        File misCosas = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\DOCS\\Mis Cosas");
+
+        System.out.println("Mis cosas añadida: "
+                +misCosas.mkdir());
 
         //crear carpeta alfabeto
-        File alfabeto = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\DOCS\\Alfabeto");
-        System.out.println(alfabeto.mkdir());
+        File alfabeto = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\DOCS\\Alfabeto");
+
+        System.out.println("Alfabeto añadido: "
+                +alfabeto.mkdir());
 
         //mover carpeta fotografias y libros dentro de mis cosas
-        File fotografias = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\FOTOS");
-        System.out.println(fotografias.renameTo(new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\DOCS\\Mis Cosas")));
+        File fotografias = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\DOCS\\FOTOS");
 
-        File libros = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\LECTURAS");
-        System.out.println(libros.renameTo(new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\DOCS\\Mis Cosas")));
+        System.out.println("Movido: "
+                +fotografias.renameTo(new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\FOTOS\\Mis Cosas")));
+
+        File libros = new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\DOCS\\LECTURAS");
+
+        System.out.println("Movido: "
+                +libros.renameTo(new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\DOCS\\Mis Cosas")));
 
         //Dentro de la carpeta alfabeto crear carpeta para cada letra del alfabeto
-        //ASCII A=65 - Z=90
-        for (int i=65; i<=90; i++){
 
+        //ASCII A=65 - Z=90
+        char alfa;
+
+        //le indicamos que empezamos en el 65 porque ahi es donde empieza en el caracter A y termina en
+        // el 90 que es donde esta la Z en el ASCII
+        for (int i=65; i<=90; i++){
+            //al pasalo a char el ya sabe que numero es en el ASCII
+            alfa= (char) i;
+
+            //creamos la carpeta
+            File carpetasAlfabeto =new File("C:\\Users\\Vanessa\\Documents\\GitHub\\Progra\\temas\\tema12\\documentos\\DOCS\\Alfabeto\\" + alfa);
+
+            System.out.println("Carpeta añadida: "
+                    +carpetasAlfabeto.mkdir());
         }
 
 
--- temas/tema12/src/ejr4/pruebas.java
+++ temas/tema12/src/ejr4/pruebas.java
@@ -2,7 +2,11 @@ package ejr4;
 
 public class pruebas {
     public static void main(String[] args) {
-        char A = 65;
-        System.out.println(A);
+        char alfa;
+
+        for (int i=65; i<=90; i++){
+            alfa= (char) i;
+            System.out.println(alfa);
+        }
     }
 }

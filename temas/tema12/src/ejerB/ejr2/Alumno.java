f02d28991dfac8ebf5a50d96528bc96949409214
Vane <vanessa-lopez9@hotmail.com>
2023-04-30 11:22:36 +0200

    2

--- temas/tema12/src/ejerB/ejr2/Alumno.java
+++ temas/tema12/src/ejerB/ejr2/Alumno.java
@@ -2,4 +2,38 @@ package ejerB.ejr2;
 
 public class Alumno {
     //atributos
+    String nombre, apellido;
+    int notaMedia;
+
+    //construcctor
+    public Alumno(String nombre, String apellido, int notaMedia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.notaMedia = notaMedia;
+    }
+
+    //getter y setters
+    //nombre
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //apellido
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    //nota media
+    public int getNotaMedia() {
+        return notaMedia;
+    }
+    public void setNotaMedia(int notaMedia) {
+        this.notaMedia = notaMedia;
+    }
 }
--- temas/tema12/src/ejerB/ejr2/ejer2B.java
+++ temas/tema12/src/ejerB/ejr2/ejer2B.java
@@ -2,30 +2,57 @@ package ejerB.ejr2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class ejer2B {
     public static void main(String[] args) {
+        //arrayList para guardar a los alumnos
+        ArrayList<Alumno> listaAlumno =new ArrayList<>();
+
         String linea;
         File f = new File("temas\\tema12\\DOCS\\alumnos_notas.txt");
-        int notas=0,media,cont=1;
+        int notas,media,cont;
 
         try(Scanner entrada = new Scanner(f)){
             while (entrada.hasNextLine()){
                 linea= entrada.nextLine();
 
                 String[] partes = linea.split(" ");
-                System.out.printf("%nNombre: %s %nApellido: %s %n", partes[0], partes[1]);
+                notas=0;cont=0;
 
-                for(int i = Integer.parseInt(partes[2]); i <= partes.length; i++){
-                    System.out.println(i);
-                    notas = notas + i;
+                for(int i = 2; i < partes.length; i++){
+                    notas = notas+Integer.parseInt(partes[i]);
                     cont++;
                 }
                 media=notas/cont;
 
-                System.out.println("Nota media: "+media);
-                System.out.println("-------------");
+                //cramos los alumnos y los guardamos
+                listaAlumno.add(new Alumno(partes[0],partes[1],media));
+
+                //los ordenamos por media
+                listaAlumno.sort(new Comparator<Alumno>() {
+                    @Override
+                    public int compare(Alumno a1, Alumno a2) {
+                        //nota media a1 > a2 = -1
+                        if(a1.getNotaMedia() > a2.getNotaMedia())
+                            return  -1;
+                        //nota media a2 > a1 = 1
+                        if(a1.getNotaMedia() < a2.getNotaMedia())
+                            return  1;
+                        //a1 = a2 =0
+                        else
+                            return 0;
+                    }
+                });
+
+            }
+
+            //mostramos los alumnos
+            for (Alumno a : listaAlumno) {
+                System.out.printf("%nNombre: %s, Apellido: %s, Media: %d",a.getNombre(),a.getApellido(),a.getNotaMedia());
+                System.out.printf("%n-------------------------------------------------");
             }
 
         } catch (FileNotFoundException e) {

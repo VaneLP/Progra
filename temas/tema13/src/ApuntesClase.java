76b63ec227d66238a8669f3a9c442b0f491ef246
Vanessa Lopez <vanessa-lopez9@hotmail.com>
2023-05-09 09:46:12 +0200

    Apuntes progra

---
+++ temas/tema13/src/ApuntesClase.java
@@ -0,0 +1,46 @@
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.Color;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//public class ApuntesClase {
+//
+//    public EjemploTeoria(string cadena) {
+//        JLabel etiq = new JLabel("Componente de texto");
+//        panelPrincipal.add(etiq);
+//        JLabel etiq2 = new JLabel("componente de texto 2");
+//        JButton boton = new JButton("pulsame");
+//        panelPrincipal.add(boton);
+//        boton.addActionListener(new ActionListener())
+//        { //capturamos el evento de pulsar el boton y le decimos lo que tenemos que hacer
+//
+//        }
+//        private void pulsarBoton1() {
+//            etiq.setForeground(Color.MAGENTA);
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                try{
+//                    Ejemplo
+//                }
+//            }
+//        });
+//    }
+//}
+//
+//class  ImplentarAccion implements ActionListener{
+//    @Override
+//    public void actionPerformed(ActionEvent actionEvent){
+//        System.out.println(actionEvent.getSource().getClass());
+//        //getSource nos devuelve un objeto
+//
+//        JButton boton = (JButton) actionEvent.getSource();
+//        boton.setBackground(Color.CYAN);
+//    }
+//}
---
+++ temas/tema13/src/PrimerFormularioLanzador.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class PrimerFormularioLanzador {
+    public static void main(String[] args) {
+        JFrame panel=new JFrame("ventana");
+        panel.setBounds(300,30,500,400);
+        panel.setContentPane();
+        panel.setVisible(true);
+    }
+}

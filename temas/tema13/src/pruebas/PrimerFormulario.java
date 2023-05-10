aac276613c2beb78f8ed64ccacc4f4840674cbb3
Vanessa Lopez <vanessa-lopez9@hotmail.com>
2023-05-10 13:46:08 +0200

    Correcion

--- temas/tema13/src/pruebas/PrimerFormulario.java
+++ temas/tema13/src/pruebas/PrimerFormulario.java
@@ -4,4 +4,5 @@ import javax.swing.*;
 
 public class PrimerFormulario {
     private JPanel panel; //al ponerle al .form el nombre al formulario se nos crea el atributo auto
+    private JSlider slider1;
 }

371bc401d746bc2cae3f4c5e7ac34cabae5fd72f
Vanessa Lopez <vanessa-lopez9@hotmail.com>
2023-05-17 09:49:03 +0200

    panel casi terminado

--- temas/tema13/src/swing/casoPractico/BurgerMenuApp.java
+++ temas/tema13/src/swing/casoPractico/BurgerMenuApp.java
@@ -4,10 +4,18 @@ import javax.swing.*;
 
 public class BurgerMenuApp {
     private JPanel panelPrincipal;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
+    private JRadioButton repartirADomicilioRadioButton;
+    private JRadioButton recogidaEnLocal20RadioButton;
     private JButton calcularButton;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
+    private JRadioButton polloRadioButton;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+    private JSpinner spinner4;
+    private JCheckBox hamburguesaDoble2€CheckBox;
+    private JCheckBox extraDeQueso0CheckBox;
+    private JCheckBox extraDePatatas1€CheckBox;
 }

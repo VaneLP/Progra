0dbb7a304bbce5931658213d38720391993cc616
Vanessa Lopez <vanessa-lopez9@hotmail.com>
2023-05-22 09:23:27 +0200

    casita terminada

--- temas/tema13/src/Pintando/Casita.java
+++ temas/tema13/src/Pintando/Casita.java
@@ -26,13 +26,41 @@ class Lamina extends JPanel{
         super.paintComponents(g);
         Graphics2D g2 = (Graphics2D)g;
 
-
+        //Cuadrado (CIELO)
+        g2.setColor(Color.CYAN);
+        g2.fillRect(0,0,400,400);
 
         //circulo (SOL)
         g2.setColor(Color.YELLOW);
         g2.fillOval(-40,-40,100,100);
 
+        //rectangulo (SUELO)
+        g2.setColor(Color.GREEN);
+        g2.fillRect(0,330,400,50);
 
+        //cuadrado (CASA)
+        g2.setColor(Color.PINK);
+        g2.fillRect(120,150,180,180);
+
+        //rectangulo (PUERTA)
+        g2.setColor(new Color(0xA35224));
+        g2.fillRect(160,240,40,90);
+
+        //cuadrado ventana
+        g2.setColor(Color.WHITE);
+        g2.fillRect(230,180,50,50);
+
+        //rectangulo (CHIMENEA)
+        g2.setColor(Color.BLACK);
+        g2.fillRect(250,50,40,90);
+
+        //triangulo (TEJADO)
+        g2.setColor(Color.RED);
+        //para hacer un poligono necesitamos un int[] x, un int[] y y los puntos que va a tener
+        int[] x = {120,210,300};
+        int[] y = {150,50,150};
+
+        g2.fillPolygon(x,y,3);
 
     }
 }

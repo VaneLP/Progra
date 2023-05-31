9794c5bf143e7c366f95f93fb25d141ef6d25e2b
Vane <vanessa-lopez9@hotmail.com>
2023-05-31 17:36:41 +0200

    avanzando

--- temas/tema14/src/main/java/Ejercicio_CRUD/CiudadesDao.java
+++ temas/tema14/src/main/java/Ejercicio_CRUD/CiudadesDao.java
@@ -1,15 +1,47 @@
 package Ejercicio_CRUD;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 public class CiudadesDao implements DAO<Ciudades>{
     //para obtener las conexiones con la base de datos
     private DataSource dataSource;
+
+    //constructor
+    public CiudadesDao() {
+        //creamos el objeto properties
+        Properties datos = new Properties();
+
+        //cargamos el properties
+        try (InputStream configStream = CiudadesDao.class.getClassLoader().getResourceAsStream("dataBase.properties")) {
+            //en el objeto de properties "datos" metemos toddos los datos que tiene el fichero
+            datos.load(configStream);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //creamos un basicDataSource
+        BasicDataSource basicDataSource = new BasicDataSource();
+
+        //le asignamos la URL con los datos que tenemos en el fichero properties
+        basicDataSource.setUrl("jdbc:mysql://" + datos.getProperty("db.servidor") + ":3306/" + datos.getProperty("db.baseDatos"));
+        //le asignamos el usuario y la clave con los datos del fuchero properties
+        basicDataSource.setUsername(datos.getProperty("db.usuario"));
+        basicDataSource.setPassword(datos.getProperty("db.clave"));
+
+        this.dataSource = basicDataSource;
+    }
+
 
     //metodos herados de DAO
     /**

15061b6951b8015054e23d34cbeb90fa1bbc6570
Vanessa Lopez <vanessa-lopez9@hotmail.com>
2023-06-07 08:24:01 +0200

    repaso

---
+++ Repaso/src/bucles/Ejer5.java
@@ -0,0 +1,21 @@
+package bucles;
+
+import java.util.Scanner;
+
+/*Realiza un programa que lea un número positivo N y calcule y visualice su factorial N! Siendo el factorial:
+0! = 1
+1! = 1
+2! = 2 * 1
+3! = 3 * 2* 1
+N! = N * (N-1) * (N-2) * … * 1
+*/
+public class Ejer5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("dime un numero positivo");
+        int n = entrada.nextInt();
+
+
+    }
+}
---
+++ Repaso/src/bucles/Ejer6.java
@@ -0,0 +1,23 @@
+package bucles;
+
+import java.util.Scanner;
+
+//Realiza un programa que lea 10 números no nulos y luego muestre un mensaje de si ha leído algún número negativo o no.
+public class Ejer6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero, contNeg=0;
+
+        System.out.println("dime 10 numero");
+
+        for(int i = 1; i<=10;i++){
+            numero = entrada.nextInt();
+
+            if(numero<0)
+                contNeg++;
+        }
+
+        System.out.println("numeros neg= "+contNeg);
+
+    }
+}
